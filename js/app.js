aeeb3a939b0df5579a473fa25374f2bf850b3b3c
MRAnsari <32181616+MRAnsari@users.noreply.github.com>
2017-11-04 13:51:37 -0700

    Hours of Operation Index
    Made the hours of operation nice to look at by having it only show today's hours using Javascript

--- js/app.js
+++ js/app.js
@@ -42,3 +42,47 @@ if (dayOfWeek == 0)
 
 document.getElementById('todaysevent').innerHTML = todayevent;
 // Today's Event Switcher END
+
+// Hours of Operation Switcher START
+// var today = new Date();
+// var dayOfWeek = today.getDay();
+
+  // console.log(today);
+  // console.log(dayOfWeek);
+
+var todayhoo;
+
+// Sunday
+if (dayOfWeek == 0)
+    { todayhoo = 'Sunday&colon; 12&colon;00pm &mdash; 9&colon;00pm'; }
+
+// Monday
+  else if (dayOfWeek == 1)
+    { todayhoo = 'Monday&colon; 12&colon;00pm &mdash; 10&colon;00pm'; }
+
+// Tuesday
+  else if (dayOfWeek == 2)
+    { todayhoo = 'Tuesday&colon; 12&colon;00pm &mdash; 10&colon;00pm'; }
+
+// Wednesday
+  else if (dayOfWeek == 3)
+    { todayhoo = 'Wednesday&colon; 12&colon;00pm &mdash; 12&colon;00pm'; }
+
+// Thursday
+  else if (dayOfWeek == 4)
+    { todayhoo = 'Thursday&colon; 12&colon;00pm &mdash; 10&colon;00pm'; }
+
+// Friday
+  else if (dayOfWeek == 5)
+    { todayhoo = 'Friday&colon; 12&colon;00pm &mdash; 12&colon;00pm'; }
+
+// Saturday
+  else if (dayOfWeek == 6)
+    { todayhoo = 'Saturday&colon; 12&colon;00pm &mdash; 11&colon;00pm'; }
+
+  else
+    { todayhoo = 'Hours of Operation on About Page'; }
+
+document.getElementById('todayshoo').innerHTML = todayhoo;
+
+// Hours of Operation Switcher END
